a6bc6395a9d4b65f6e2ddfd057e6ece951965b55
Victor M. Rivas <vrivas@ujaen.es>
2022-05-11 17:36:45 +0200

    Terminada función calcular _Y_dado_T

--- src/main/java/es/ujaen/dagil/App.java
+++ src/main/java/es/ujaen/dagil/App.java
@@ -2,7 +2,8 @@ package es.ujaen.dagil;
 
 /**
  * Cálculo de fórmulas para tiro parabólico SIN rozamiento, CON gravedad.
- * Calculadas según: https://www.areaciencias.com/fisica/tiro-parabolico-formulas/ 
+ * Calculadas según:
+ * https://www.areaciencias.com/fisica/tiro-parabolico-formulas/
  *
  */
 public class App {
@@ -21,7 +22,8 @@ public class App {
      * 
      */
 
-    public static double calcular_X_dado_T(final double x_ini, final double v_ini, final double angulo, final double t) {
+    public static double calcular_X_dado_T(final double x_ini, final double v_ini, final double angulo,
+            final double t) {
         if (t < 0)
             throw (new ArithmeticException("El tiempo no puede ser negativo."));
         if (angulo < 0 || angulo > Math.PI / 2)
@@ -32,7 +34,6 @@ public class App {
         return x;
     }
 
-
     /**
      * Calcula el tiempo que tarda el proyectil en alcanzar un determinado punto X
      * Consideramos el rozamiento nulo.
@@ -41,18 +42,56 @@ public class App {
      * @param v_ini  Velocidad inicial
      * @param angulo Ángulo (expresado en radianes) con el que se lanza el
      *               proyectil.
-     * @param x_fin  Posición final X para la que se quiere saber el tiempo en que la alcanzará.
-     * @post Si el coseno del angulo es 0, lanza una excepción de tipo ArithmeticException
+     * @param x_fin  Posición final X para la que se quiere saber el tiempo en que
+     *               la alcanzará.
+     * @post Si el coseno del angulo es 0, lanza una excepción de tipo
+     *       ArithmeticException
      * 
      */
 
-    public static double calcular_T_dado_X(final double x_ini, final double v_ini, final double angulo, final double x_fin) {
+    public static double calcular_T_dado_X(final double x_ini, final double v_ini, final double angulo,
+            final double x_fin) {
 
-        //double x = x_ini + v_ini * Math.cos(angulo) * t; 
-        double t=(x_fin-x_ini)/(v_ini*Math.cos(angulo));
+        // double x = x_ini + v_ini * Math.cos(angulo) * t;
+        if (angulo < 0 || angulo > Math.PI / 2)
+            throw (new ArithmeticException("El ángulo debe estar entre 0 y PI/2"));
+        if (angulo == Math.PI / 2)
+            throw (new ArithmeticException("El ángulo no puede ser PI/2"));
+        double t = (x_fin - x_ini) / (v_ini * Math.cos(angulo));
         return t;
     }
+
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+
+    /**
+     * Calcula el desplazamiento en el eje Y durante un tiempo t.
+     * Consideramos el rozamiento nulo
+     * 
+     * @param y_ini Posición Y inicial
+     * @param v_ini Velocidad inicial
+     * @param angulo Ángulo (expresado en radianes) con el que se lanza el proyectil.
+     * @param g Valor para la gravedad (normalmente -9.8)
+     * @param t Tiempo (medido en segundos) para el que se quiere calcular Y
+     *
+     * @post Si t<0, lanza una excepción de tipo ArithmeticException
+     * @post Si angulo<0 o angulo>pi/2, lanza una excepción de tipo ArithmeticException
+     * 
+     */
+
+    public static double calcular_Y_dado_T(final double y_ini, final double v_ini, final double angulo, final double g,
+            final double t) {
+        if (t < 0)
+            throw (new ArithmeticException("El tiempo no puede ser negativo."));
+        if (angulo < 0 || angulo > Math.PI / 2)
+            throw (new ArithmeticException("El ángulo debe estar entre 0 y PI/2"));
+        if (t == 0)
+            return y_ini;
+        double v_0y = v_ini * Math.sin(angulo);
+        double y = y_ini + v_0y * t + (1 / 2.0) * g * t * t;
+        return y;
+    }
+
+
 }
--- src/test/java/es/ujaen/dagil/AppTest.java
+++ src/test/java/es/ujaen/dagil/AppTest.java
@@ -11,20 +11,20 @@ import org.junit.Test;
  */
 public class AppTest {
     @Test
-    public void calcularX() {
+    public void calcular_X_dado_T() {
         // Valores negativos de t deben lanzar una excepción
         try {
             App.calcular_X_dado_T(0, 0, 0, -1);
             fail();
         } catch (Exception e) {
-            System.err.println("calcularX: " + e.getMessage());
+            System.err.println("calcular_X_dado_T: " + e.getMessage());
         }
         // Valores del angulo que no estén entre 0 y PI/2 deben lanzar una excepción
         try {
             App.calcular_X_dado_T(0, 0, Math.PI, 0);
             fail();
         } catch (Exception e) {
-            System.err.println("calcularX: " + e.getMessage());
+            System.err.println("calcular_X_dado_T: " + e.getMessage());
         }
         // Valor de t igual a 0, debe devolver el mismo valor que x_ini
         // hacemos un par de comprobaciones
@@ -48,7 +48,7 @@ public class AppTest {
     }
 
     @Test
-    public void calcularTParaX() {
+    public void calcular_T_dado_X() {
         // Si x_ini == x_fin, t debe ser 0
         assertEquals(0, App.calcular_T_dado_X(-10, 26, Math.toRadians(40), -10), 0.1);
 
@@ -66,17 +66,51 @@ public class AppTest {
             App.calcular_T_dado_X(0, 0, Math.PI, 0);
             fail();
         } catch (Exception e) {
-            System.err.println("calcularTParaX: " + e.getMessage());
+            System.err.println("calcular_T_dado_X: " + e.getMessage());
         }
         
         // Valor PI/2 para el ángulo debe lanzar una excepción
         try {
-            App.calcular_T_dado_X(0, 0, 0, 0);
+            App.calcular_T_dado_X(0, 0, Math.PI/2, 0);
             fail();
         } catch (Exception e) {
-            System.err.println("calcularTParaX: " + e.getMessage());
+            System.err.println("calcular_T_dado_X: " + e.getMessage());
+        }        
+    }
+
+
+    @Test
+    public void calcular_Y_dado_T() {
+        // Valores negativos de t deben lanzar una excepción
+        try {
+            App.calcular_Y_dado_T(0, 0, 0, -9.8, -1);
+            fail();
+        } catch (Exception e) {
+            System.err.println("calcularX: " + e.getMessage());
         }
         
+        // Valores del angulo que no estén entre 0 y PI/2 deben lanzar una excepción
+        try {
+            App.calcular_Y_dado_T(0, 0, Math.PI,-9.8, 0);
+            fail();
+        } catch (Exception e) {
+            System.err.println("calcularX: " + e.getMessage());
+        }
+        // Valor de t igual a 0, debe devolver el mismo valor que x_ini
+        // hacemos un par de comprobaciones
+        assertEquals(10, App.calcular_Y_dado_T(10, 29, 0, -9.8, 0), 0);
+        assertEquals(4, App.calcular_Y_dado_T(4, 12, 0, -9.8, 0), 0);
 
+
+        // Según https://www.areaciencias.com/fisica/tiro-parabolico-formulas/
+
+        // Para una velocidad de 26m/s, un ángulo de 40º, y 3.41/2 segundos, Y debe
+        // devolver 14.23m
+        assertEquals(14.23, App.calcular_Y_dado_T(0, 26, Math.toRadians(40), -9.8, 3.41/2), 0.1);
+
+        // Si en vez de partir desde Y=0, damos otro valor, debe incrementarse en dicho
+        // valor
+        assertEquals(14.23+10, App.calcular_Y_dado_T(0+10, 26, Math.toRadians(40), -9.8, 3.41/2), 0.1);
+        assertEquals(14.23-10, App.calcular_Y_dado_T(0-10, 26, Math.toRadians(40), -9.8, 3.41/2), 0.1);
     }
 }
